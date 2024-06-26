384072d3ca95d26f388716869d684919114195cd
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2024-06-26 19:01:05 +0000

    feat: initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import { zenmlPreset } from "@zenml-io/react-component-library/tailwind";
+import type { Config } from "tailwindcss";
+
+export default {
+	content: [
+		"./app/**/*.{js,jsx,ts,tsx}",
+		"./node_modules/@zenml-io/react-component-library/**/*.{js,ts,jsx,tsx}"
+	],
+	theme: {
+		extend: {}
+	},
+	plugins: [],
+	presets: [zenmlPreset]
+} satisfies Config;
