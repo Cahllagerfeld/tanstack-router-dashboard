54ba6b9d3362fc30e1c7601d75b1131bf2d1686a
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2024-11-18 18:53:38 +0000

    fix: migrate to tanstack router

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,8 @@
-import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react-swc";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
+// https://vite.dev/config/
 export default defineConfig({
-	plugins: [
-		remix({
-			ssr: false,
-			future: {
-				v3_fetcherPersist: true,
-				v3_relativeSplatPath: true,
-				v3_throwAbortReason: true
-			}
-		}),
-		tsconfigPaths()
-	]
+  plugins: [TanStackRouterVite(), react()],
 });
