95876583abcacf395154316aa0fbf60b4e490caf
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2025-04-06 18:29:12 +0200

    refactor: more refactorings

--- src/lib/localstorage.ts
+++ src/lib/localstorage.ts
@@ -1,9 +1,10 @@
 const LOCALSTORAGE_PREFIX = "zenml";
 
 const LOCALSTORAGE_KEYS = {
-	SELECTED_PROJECT: "selected-project",
+	SELECTED_PROJECT: "projects.selected",
+	AUTH_USER: "auth.user",
 } as const;
 
 export function getLocalStorageKey(key: keyof typeof LOCALSTORAGE_KEYS) {
-	return `${LOCALSTORAGE_PREFIX}.${LOCALSTORAGE_KEYS[key]}`;
+	return `${LOCALSTORAGE_PREFIX}.${LOCALSTORAGE_KEYS[key]}` as const;
 }
