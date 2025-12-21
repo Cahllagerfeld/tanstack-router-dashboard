8cffacd6f3944680e41a61d567ad99a9e89ab514
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2025-12-21 16:20:26 +0100

    feat: grid item

--- src/features/stacks/create/component-type-grid.tsx
+++ src/features/stacks/create/component-type-grid.tsx
@@ -1,6 +1,6 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { COMPONENT_TYPES } from "@/lib/constants/component-types";
-import { snakeCaseToTitleCase } from "@/lib/strings";
+import { ComponentTypeGridItem } from "./component-type-grid-item";
 
 export function ComponentTypeGrid() {
 	return (
@@ -8,11 +8,9 @@ export function ComponentTypeGrid() {
 			<CardHeader>
 				<CardTitle>Stack Components</CardTitle>
 			</CardHeader>
-			<CardContent className="grid grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-4">
+			<CardContent className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
 				{COMPONENT_TYPES.map((type) => (
-					<div className="rounded-sm border p-2 text-sm">
-						{snakeCaseToTitleCase(type)}
-					</div>
+					<ComponentTypeGridItem key={type} type={type} />
 				))}
 			</CardContent>
 		</Card>
--- src/lib/constants/component-types.ts
+++ src/lib/constants/component-types.ts
@@ -2,18 +2,18 @@ import { ComponentType } from "@/types/components";
 
 // generate an array with all possible values from ComponentType
 export const COMPONENT_TYPES = [
+	"orchestrator",
+	"artifact_store",
+	"deployer",
+	"container_registry",
 	"alerter",
 	"annotator",
-	"artifact_store",
-	"container_registry",
 	"data_validator",
 	"experiment_tracker",
 	"feature_store",
 	"image_builder",
 	"model_deployer",
 	"model_registry",
-	"orchestrator",
 	"step_operator",
-	"deployer",
 	"log_store",
 ] as const satisfies ComponentType[];
