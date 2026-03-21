672438daae0291c678a6d6a859235e026d491f16
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2026-03-21 11:22:19 +0100

    refactor: migrate vite 8 (#217)

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,25 @@
 /// <reference types="vitest/config" />
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { tanstackRouter } from "@tanstack/router-plugin/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
 	test: {
 		exclude: ["node_modules/**/*", "build/**/*", "dist/**/*", "e2e-tests/**/*"],
 	},
+	resolve: {
+		tsconfigPaths: true,
+	},
 	plugins: [
 		tanstackRouter({
 			autoCodeSplitting: true,
 		}),
-		react({
-			babel: {
-				plugins: ["babel-plugin-react-compiler"],
-			},
-		}),
+		react(),
+		babel({ presets: [reactCompilerPreset()] }),
 		tailwindcss(),
-		tsconfigPaths(),
 	],
 	server: {
 		proxy: {
