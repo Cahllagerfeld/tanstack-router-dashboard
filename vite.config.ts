bf9f9c78fe76ec49116236b7b6181ccee6005f2e
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2026-03-22 16:54:14 +0100

    feat: env schema

--- vite.config.ts
+++ vite.config.ts
@@ -3,31 +3,44 @@ import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+import { envSchema } from "./src/features/core/env-schema.ts";
 
 // https://vite.dev/config/
-export default defineConfig({
-	test: {
-		exclude: ["node_modules/**/*", "build/**/*", "dist/**/*", "e2e-tests/**/*"],
-	},
-	resolve: {
-		tsconfigPaths: true,
-	},
-	plugins: [
-		tanstackRouter({
-			autoCodeSplitting: true,
-		}),
-		react(),
-		babel({ presets: [reactCompilerPreset()] }),
-		tailwindcss(),
-	],
-	server: {
-		proxy: {
-			"/api": {
-				target: "http://localhost:8237",
-				changeOrigin: true,
-				secure: false,
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), "");
+	const parsedEnv = envSchema.parse(env);
+
+	const backendUrl = parsedEnv.VITE_BACKEND_URL;
+
+	return {
+		test: {
+			exclude: [
+				"node_modules/**/*",
+				"build/**/*",
+				"dist/**/*",
+				"e2e-tests/**/*",
+			],
+		},
+		resolve: {
+			tsconfigPaths: true,
+		},
+		plugins: [
+			tanstackRouter({
+				autoCodeSplitting: true,
+			}),
+			react(),
+			babel({ presets: [reactCompilerPreset()] }),
+			tailwindcss(),
+		],
+		server: {
+			proxy: {
+				"/api": {
+					target: backendUrl,
+					changeOrigin: true,
+					secure: false,
+				},
 			},
 		},
-	},
+	};
 });
