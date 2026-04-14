c2903bc5ed4966d61f9ab6e7b3aa5f22c9aad6d3
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2026-04-14 19:11:24 +0200

    fix: ci

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
 		paraglideVitePlugin({
 			project: "./project.inlang",
 			outdir: "./src/paraglide",
+			strategy: ["preferredLanguage", "baseLocale"],
 		}),
 		tanstackRouter({
 			autoCodeSplitting: true,
