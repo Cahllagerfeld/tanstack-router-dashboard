c03fe905609d805416de2823e38f3ff346d1d8fc
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2026-05-02 20:29:38 +0200

    chore(schemas): Update Zod Schema (#281)
    * refactor(schema): use literal
    
    * chore: make int

--- src/features/filters/common-filter-schema.ts
+++ src/features/filters/common-filter-schema.ts
@@ -2,13 +2,5 @@ import { z } from "zod";
 
 export const commonFilterSchema = z.object({
 	page: z.number().int().positive().optional().catch(undefined),
-	size: z
-		.number()
-		.int()
-		.positive()
-		.optional()
-		.transform((val) => {
-			if (val === undefined) return undefined;
-			if ([5, 10, 20, 50].includes(val)) return val as 5 | 10 | 20 | 50;
-		}),
+	size: z.literal([5, 10, 20, 50, 100]).optional().catch(undefined),
 });
